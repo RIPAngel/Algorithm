53657fefe0639599e9f268265c372318fed58d4e
fxrcha <truetype24@gmail.com>
2020-06-01 23:34:53 +0900

    1

---
+++ helloalgo/counerweight.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int n, data[] = {1, 3, 9, 27, 81, 243, 729}, end;
+bool chk[8];
+
+void f (int num, int sum) {
+    if (end) return;
+    if (sum == num) {
+        
+    }
+}
